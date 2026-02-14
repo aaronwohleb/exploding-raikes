917465af85b67a0bf937821d0f61b951579814c7
Garrett Moore <garrmoor314@gmail.com>
2026-02-14 17:36:19 -0600

    Finished Server Architecture (Game)
    Added the remaining functions and documentation for the System Architecture

--- server/game-runner/DiscardPile.ts
+++ server/game-runner/DiscardPile.ts
@@ -1,12 +1,41 @@
+/**
+ * Represents a deck of Exploding Kittens cards with the functionality of the game's discard pile.
+ */
 class DiscardPile {
 
-    private pile: Card[];
+    private _pile: Card[];
 
+    /**
+     * Constructs a DiscardPile object with an empty pile
+     */
     public constructor() {
-        this.pile = [];
+        this._pile = [];
     }
 
+    /**
+     * Allows the specified player to draw a card of their choice from the discard pile.
+     * 
+     * @param player the player that will draw the card
+     */
     public drawFromDiscard(player: Player) {
         // draw card of choice from DCP
+    }
+
+    /**
+     * Gets the DiscardPile object's pile.
+     * 
+     * @return the Card's type
+     */
+    public get pile(): Card[] {
+        return this._pile;
+    }
+
+    /**
+     * Sets the DiscardPile object's pile.
+     * 
+     * @param value the edited pile.
+     */
+    public set pile(value: Card[]) {
+        this._pile = value;
     }
 }
--- server/game-runner/DrawDeck.ts
+++ server/game-runner/DrawDeck.ts
@@ -1,9 +1,15 @@
+/**
+ * Represents a deck of Exploding Kittens cards with the functionality of the game's draw pile.
+ */
 class DrawDeck {
 
-    private deck: Card[];
+    private _deck: Card[];
 
+    /**
+     * Builds a full deck of Exploding Kittens Cards.
+     */
     public constructor() {
-        this.deck = [];
+        this._deck = [];
         // Make full deck of cards
         for (let i: number = 0; i < 4; i++) {
             this.deck.push(new Card(CardType.Attack, i, "Generic"));
@@ -46,15 +52,48 @@ class DrawDeck {
         }
     }
 
+    /**
+     * When an Exploding Kitten is drawn and defused, this function puts the card back into the draw pile at the location of the player's choice.
+     * 
+     * @param kitten the Exploding Kitten that was drawn
+     * @param index the spot in the deck to replace it
+     */
     public replaceExplodingKitten(kitten: Card, index: number) {
         // Insert Kitten into deck
     }
 
+    /**
+     * This function shows the top numCards cards to the player.
+     * 
+     * @param numCards the number of cards to show
+     * @returns the array of cards to show
+     */
     public seeFuture(numCards: number): Card[] {
         return [this.deck[0]];
     }
 
+    /**
+     * This function shuffles the deck of cards.
+     */
     public shuffleDeck() {
         // Shuffle the deck
+    }
+
+    /**
+     * Gets the DrawDeck object's deck.
+     * 
+     * @return the DrawDeck object's deck
+     */
+    public get deck(): Card[] {
+        return this._deck;
+    }
+
+    /**
+     * Sets the DrawDeck object's deck.
+     * 
+     * @param value the edited DrawDeck object's deck
+     */
+    public set deck(value: Card[]) {
+        this._deck = value;
     }
 }
